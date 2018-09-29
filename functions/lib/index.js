60b780d62cc0b177a9430d57233ad28d34c1b376
Damoon <damoon.rashidi@gmail.com>
2018-09-29 22:03:56 +0200

    add hn posts

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -10,15 +10,35 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 const functions = require("firebase-functions");
 const node_fetch_1 = require("node-fetch");
-exports.listings = functions.https.onRequest((_, res) => __awaiter(this, void 0, void 0, function* () {
-    const API = `https://www.reddit.com/user/tehrash/m/work.json`;
-    const data = (yield (yield node_fetch_1.default(API)).json()).data.children.map(r => r.data);
-    const posts = data.map(post => ({
+const mergePosts = (...posts) => posts.sort((a, b) => (a.points > b.points ? -1 : 1));
+const getReddit = () => __awaiter(this, void 0, void 0, function* () {
+    const redditAPI = `https://www.reddit.com/user/tehrash/m/work.json`;
+    const data = (yield (yield node_fetch_1.default(redditAPI)).json()).data.children.map(r => r.data);
+    return data.map(post => ({
         title: post.title,
         author: post.author,
         points: post.ups - post.downs,
-        category: post.subreddit
+        category: post.subreddit,
+        url: post.url,
+        comments: post.num_comments,
     }));
+});
+const getHN = () => __awaiter(this, void 0, void 0, function* () {
+    const hnAPI = `https://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=points%3E50`;
+    const data = (yield (yield node_fetch_1.default(hnAPI)).json()).hits;
+    return data.map((post) => ({
+        title: post.title,
+        author: post.author,
+        points: post.points,
+        category: 'HN',
+        url: post.url,
+        comments: post.num_comments,
+    }));
+});
+exports.listings = functions.https.onRequest((_, res) => __awaiter(this, void 0, void 0, function* () {
+    const redditPosts = yield getReddit();
+    const hnPosts = yield getHN();
+    const posts = mergePosts(...redditPosts, ...hnPosts);
     res.send(posts);
 }));
 //# sourceMappingURL=index.js.map
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,43 @@
 import * as functions from 'firebase-functions';
 import fetch from 'node-fetch';
-import { Post } from './interfaces/post.interface';
+import { RedditPost, Post, HNPost } from './interfaces/post.interface';
 
-export const listings = functions.https.onRequest(async (_, res) => {
-  const API = `https://www.reddit.com/user/tehrash/m/work.json`;
-  const data: Post[] = (await (await fetch(API)).json()).data.children.map(
-    r => r.data
-  );
-  const posts = data.map((post: Post) => ({
+const mergePosts = (...posts: Post[]): Post[] =>
+  posts.sort((a, b) => (a.points > b.points ? -1 : 1));
+
+const getReddit = async () => {
+  const redditAPI = `https://www.reddit.com/user/tehrash/m/work.json`;
+  const data: RedditPost[] = (await (await fetch(
+    redditAPI
+  )).json()).data.children.map(r => r.data);
+
+  return data.map(post => ({
     title: post.title,
     author: post.author,
     points: post.ups - post.downs,
     category: post.subreddit,
     url: post.url,
+    comments: post.num_comments,
   }));
+};
+
+const getHN = async () => {
+  const hnAPI = `https://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=points%3E50`;
+  const data = (await (await fetch(hnAPI)).json()).hits;
+
+  return data.map((post: HNPost) => ({
+    title: post.title,
+    author: post.author,
+    points: post.points,
+    category: 'HN',
+    url: post.url,
+    comments: post.num_comments,
+  }));
+};
+
+export const listings = functions.https.onRequest(async (_, res) => {
+  const redditPosts = await getReddit();
+  const hnPosts = await getHN();
+  const posts = mergePosts(...redditPosts, ...hnPosts);
   res.send(posts);
 });
